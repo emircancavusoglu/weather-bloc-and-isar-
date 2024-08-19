e504cab852efd96804c4fd41472c62fe17dbb86c
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2024-08-19 16:49:10 +0300

    fetch data

--- lib/view/my_home_page_view.dart
+++ lib/view/my_home_page_view.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import '../weather/bloc/weather_bloc.dart';
-import '../weather/bloc/weather_event.dart';
-import '../weather/bloc/weather_state.dart';
-import '../widget/alert_dialog.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
+
+import '../service/model/WeatherModel.dart';
+import '../service/weather_api.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -15,47 +16,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var weatherPath = WeatherApi.weatherApi;
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      showDialog(
-        context: context,
-        builder: (context) => const PermissionAlertDialog(),
-      );
-    });
+    fetchData();
   }
+
+  Future<void> fetchData()async{
+    final response1 = await Dio().get(weatherPath);
+    final response = await Dio().get(weatherPath);
+    if(response.statusCode == 200){
+      final weather = WeatherModel.fromJson(response1.data['current']);
+      print("obje : ${weather.tempC}");
+      print("obje : ${weather.isDay}");
+      print("obje : ${weather.lastUpdatedEpoch}");
+      print("obje : ${weather.tempC}");
+    }
+    if(response.statusCode == 200){
+      final weather2 = WeatherModel.fromJson(response.data["location"]);
+      print(weather2.localtime);
+      print(weather2.region);
+    }
+  }
+  List<WeatherModel>? _items;  // bu veri gelmeyebilir
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).canvasColor,
-        title: Text(widget.title),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<WeatherBloc, WeatherState>(
-              builder: (context, state) {
-                if (state is WeatherLoadSuccess) {
-                  return state.icon;
-                } else if (state is WeatherInitial) {
-                  return const Text("Loading...", style: TextStyle(fontSize: 24));
-                } else {
-                  return const Text("Unknown state", style: TextStyle(fontSize: 24));
-                }
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<WeatherBloc>().add(WeatherLoaded());
-        },
-        child: const Icon(Icons.cloud),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Icon(Icons.cloud, color: Colors.blue,size: 80,),
+          ),
+        Text("London"),
+        Text("15 degree"),
+        Text("15 degree"),
+        ],
       ),
     );
   }
