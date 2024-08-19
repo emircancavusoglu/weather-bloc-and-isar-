ac04da75e670a010cd37abd8fbe868d1d6f6dabd
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2024-08-19 17:05:23 +0300

    basic design added

--- lib/view/my_home_page_view.dart
+++ lib/view/my_home_page_view.dart
@@ -28,15 +28,10 @@ class _MyHomePageState extends State<MyHomePage> {
     final response = await Dio().get(weatherPath);
     if(response.statusCode == 200){
       final weather = WeatherModel.fromJson(response1.data['current']);
-      print("obje : ${weather.tempC}");
-      print("obje : ${weather.isDay}");
-      print("obje : ${weather.lastUpdatedEpoch}");
-      print("obje : ${weather.tempC}");
+
     }
     if(response.statusCode == 200){
       final weather2 = WeatherModel.fromJson(response.data["location"]);
-      print(weather2.localtime);
-      print(weather2.region);
     }
   }
   List<WeatherModel>? _items;  // bu veri gelmeyebilir
@@ -44,18 +39,22 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
+        title: const Center(
+            child: Text("Yerel Saat : 15:20")),
         backgroundColor: Colors.blue,
       ),
-      body: Column(
+      body: const Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Text("LONDON"),
           Center(
-              child: Icon(Icons.cloud, color: Colors.blue,size: 80,),
+              child: Icon(Icons.cloud, color: Colors.white,size: 80,),
           ),
-        Text("London"),
-        Text("15 degree"),
-        Text("15 degree"),
+          Text("15 degree"),
+          Text("Party Cloud"),
+          Text("Last updated at : 14:55")
         ],
       ),
     );
